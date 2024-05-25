77683312aff3bbc5fb9741ad8168259599d0e1c5
Aman Jain <amansjain.aj@gmail.com>
2024-05-26 00:15:39 +0530

    setup queryClient provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,24 @@ import 'antd/dist/reset.css';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ConfigProvider } from 'antd';
+
+const queryClient = new QueryClient({});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <ConfigProvider
+                theme={{
+                    token: {
+                        colorPrimary: '#1890ff',
+                        colorLink: '#1890ff',
+                    },
+                }}
+            >
+                <RouterProvider router={router} />
+            </ConfigProvider>
+        </QueryClientProvider>
     </React.StrictMode>,
 );
