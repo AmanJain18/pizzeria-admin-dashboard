e0f3a39cb2b277ae40525203502c699b871b3817
Aman Jain <amansjain.aj@gmail.com>
2024-05-26 07:46:13 +0530

    add protected route logic

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
 import { Layout } from 'antd';
+import { useAuthStore } from '../store';
 
 const Dashboard = () => {
+    const { user } = useAuthStore();
+
+    if (user === null) {
+        return <Navigate to='/auth/login' replace={true} />;
+    }
+
     return (
         <Layout>
             <Layout.Content>
--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,7 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { Layout } from 'antd';
+import { useAuthStore } from '../store';
 
 const PublicLayout = () => {
+    const { user } = useAuthStore();
+
+    if (user !== null) {
+        return <Navigate to='/' replace={true} />;
+    }
     return (
         <Layout>
             <Layout.Content>
